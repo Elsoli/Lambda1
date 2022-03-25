4d4343cb908898c69ad536a99e4f3a92ee4f0127
Roman Novoselov <bigron@mail.ru>
2022-03-25 10:30:53 +0300

    zero check

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -6,13 +6,8 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        int c = 0;
-        try {
-            c = calc.devide.apply(a, b);
-        } catch (Exception e) {
-            System.out.println("Ошибка! Деление на ноль!");
-            ;
-        }
-        calc.println.accept(c);
+        int c = (b == 0) ? 0 : calc.devide.apply(a, b); // Проверка деления на ноль. В случае, если делитель равен нулю,
+        calc.println.accept(c);                         // для того чтобы программа не завершилась с ошибкой в качестве
+                                                        // результата возвращаем 0.
     }
 }
