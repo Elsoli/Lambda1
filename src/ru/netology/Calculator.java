fe687607e9a6a59f7c2c77f5ea7b17dc5c5f418a
Roman Novoselov <bigron@mail.ru>
2022-04-03 12:39:00 +0300

    edit devide

--- src/ru/netology/Calculator.java
+++ src/ru/netology/Calculator.java
@@ -7,9 +7,17 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? 0 : x / y;
+ /*   BinaryOperator<Integer> devide = (x, y) -> {
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
+            return 0;
+        }
+    };*/
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
     Consumer<Integer> println = System.out::println;
 }
+
--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -6,8 +6,7 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        int c = (b == 0) ? 0 : calc.devide.apply(a, b); // Проверка деления на ноль. В случае, если делитель равен нулю,
-        calc.println.accept(c);                         // для того чтобы программа не завершилась с ошибкой в качестве
-                                                        // результата возвращаем 0.
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
     }
 }
